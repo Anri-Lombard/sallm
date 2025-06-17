e5750a219df57ac17bb08149bf897619344a4ca2
Anri-Lombard <anri.m.lombard@gmail.com>
2025-06-17 03:37:07 +0200

    mamba model and sweep

--- src/main/sallm/models/registry.py
+++ src/main/sallm/models/registry.py
@@ -1,9 +1,10 @@
 from typing import Dict, Type
 
-from transformers import LlamaConfig, PretrainedConfig
+from transformers import LlamaConfig, PretrainedConfig, Mamba2Config
 
 # TODO add mamba
 # TODO consider mixtral?
 MODEL_CONFIG_REGISTRY: Dict[str, Type[PretrainedConfig]] = {
     "llama": LlamaConfig,
+    "mamba2": Mamba2Config,
 }
